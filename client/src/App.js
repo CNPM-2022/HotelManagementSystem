e92e7872c1084c28e815f30081ca559885dafa32
20120557 <81631701+orgball2608@users.noreply.github.com>
2022-10-30 11:18:51 +0700

    add private router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import Home from "./booking/Home";
-import Login from "./auth/Login";
-import Register from "./auth/Register";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TopNav from "./components/TopNav";
 import { ToastContainer } from "react-toastify";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+//component
+import Home from "./booking/Home";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import Dashboard from "./user/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -15,6 +19,9 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
+        <Route exact path="/" element={<PrivateRoute />}>
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,12 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRoute = () => {
+  const { isAuthenticated, token } = useSelector((state) => state.auth);
+
+  const isLogin = isAuthenticated && token;
+
+  return isLogin ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
