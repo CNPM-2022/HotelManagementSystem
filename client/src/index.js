fe62f6d5bfff7e27971f0e5e99f3ff03b80c5134
TAI VAN <taivannho5a@gmail.com>
2022-11-01 22:23:19 +0700

    Tat react strictmode

--- client/src/index.js
+++ client/src/index.js
@@ -9,9 +9,9 @@ import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
+    // <React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    // </React.StrictMode>,
 );
