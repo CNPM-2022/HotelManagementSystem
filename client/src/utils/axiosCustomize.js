88b188d55aeab7ddcc9f75ca54ae14877c622e4d
TAI VAN <taivannho5a@gmail.com>
2022-11-04 17:20:25 +0700

    Create new user

--- client/src/utils/axiosCustomize.js
+++ client/src/utils/axiosCustomize.js
@@ -26,8 +26,9 @@ instance.interceptors.response.use(
     function (error) {
         // Any status codes that falls outside the range of 2xx cause this function to trigger
         // Do something with response error
-        // return error.response?.data;
-        return Promise.reject(error);
+        console.log(error);
+        return error.response?.data;
+        // return Promise.reject(error);
     },
 );
 
