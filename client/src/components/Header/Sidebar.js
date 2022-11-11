e4262311c1d03c1732bb8198dbf2d08d488c62ac
TAI VAN <taivannho5a@gmail.com>
2022-11-11 16:54:27 +0700

    Design Main Header

---
+++ client/src/components/Header/Sidebar.js
@@ -0,0 +1,46 @@
+import { ProSidebar, Menu, MenuItem, SidebarHeader, SidebarContent, SidebarFooter } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Link } from 'react-router-dom';
+import { GrClose } from 'react-icons/gr';
+import { BsArrowRight } from 'react-icons/bs';
+
+function Sidebar({ collapsed, setCollapsed }) {
+    return (
+        <ProSidebar collapsed={collapsed}>
+            <SidebarHeader className="text-center">
+                <span className="close-btn" onClick={() => setCollapsed(true)}>
+                    <GrClose />
+                </span>
+            </SidebarHeader>
+            <SidebarContent>
+                <Menu>
+                    <MenuItem>
+                        Home
+                        <Link to="/" />
+                    </MenuItem>
+                    <MenuItem>
+                        Rooms
+                        <Link to="/rooms" />
+                    </MenuItem>
+                    <MenuItem>
+                        About Us
+                        <Link to="/about-us" />
+                    </MenuItem>
+                    <MenuItem>
+                        Contact
+                        <Link to="/contact" />
+                    </MenuItem>
+                </Menu>
+
+                <div className="book-now">
+                    <Link to="/book">
+                        Book Now <BsArrowRight />
+                    </Link>
+                </div>
+            </SidebarContent>
+            <SidebarFooter></SidebarFooter>
+        </ProSidebar>
+    );
+}
+
+export default Sidebar;
