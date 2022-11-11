317c1298f79a265bd8f48273a7bf9b54ec82824b
20120583 <trananhthi56@gmail.com>
2022-11-11 18:37:19 +0700

    Defaul commit

--- client/src/auth/Register/Register.js
+++ client/src/auth/Register/Register.js
@@ -15,7 +15,7 @@ const Regiter = () => {
     const dispatch = useDispatch();
 
     const registerHandler = async (userInfor) => {
-        console.log(userInfor)
+
         const { userName, name, email, phone, password } = userInfor;
 
         try {
