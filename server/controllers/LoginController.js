8f1ff0cca348acb451fec1ab19b211213ac4ed6c
20120557 <81631701+orgball2608@users.noreply.github.com>
2022-11-13 21:00:06 +0700

    feat(login) add id field for res

--- server/controllers/LoginController.js
+++ server/controllers/LoginController.js
@@ -1,54 +1,47 @@
-const argon2 = require("argon2");
-const jwt = require("jsonwebtoken");
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 
-const User = require("../models/User");
+const User = require('../models/User');
 
 // @route   POST api/auth/login
 // @desc    login user
 // @access  Public
 
 const LoginController = async (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password)
-    return res
-      .status(400)
-      .json({ success: false, message: "Missing username and/or password" });
+    const { username, password } = req.body;
+    if (!username || !password)
+        return res.status(400).json({ success: false, message: 'Missing username and/or password' });
 
-  try {
-    // Check for existing user
-    const user = await User.findOne({ username });
-    if (!user) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Incorrect username or password" });
+    try {
+        // Check for existing user
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(400).json({ success: false, message: 'Incorrect username or password' });
+        }
+        const passwordValid = await argon2.verify(user.password, password);
+        if (!passwordValid)
+            return res.status(400).json({
+                success: false,
+                message: 'Incorrect username or password',
+            });
+
+        // All good
+        const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET);
+
+        res.json({
+            success: true,
+            message: 'User logged in successfully',
+            accessToken,
+            user: {
+                id: user._id,
+                username: user.username,
+                createdAt: user.createdAt,
+                updatedAt: user.updatedAt,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Internal server error' });
     }
-    const passwordValid = await argon2.verify(user.password, password);
-    if (!passwordValid)
-      return res.status(400).json({
-        success: false,
-        message: "Incorrect username or password",
-      });
-
-    // All good
-    const accessToken = jwt.sign(
-      { userId: user._id },
-      process.env.ACCESS_TOKEN_SECRET
-    );
-
-    res.json({
-      success: true,
-      message: "User logged in successfully",
-      accessToken,
-      user: {
-        username: user.username,
-        createdAt: user.createdAt,
-        updatedAt: user.updatedAt,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: "Internal server error" });
-  }
 };
 
 module.exports = LoginController;
