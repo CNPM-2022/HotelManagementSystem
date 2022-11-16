6cb7bdcada69c7c07aff66a3389f64fcf8c675ae
TAI VAN <taivannho5a@gmail.com>
2022-11-16 22:07:43 +0700

    Design Home About

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import './Home.scss';
 import Slider from './components/Slider/Slider';
+import About from './components/About/About';
 
 const Home = () => {
     return (
         <div className="home-container">
             <Slider />
+            <About />
         </div>
     );
 };
