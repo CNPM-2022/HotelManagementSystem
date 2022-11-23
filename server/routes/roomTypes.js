e6b546ba26367e1063096777c2155f059c4448d5
20120557 <81631701+orgball2608@users.noreply.github.com>
2022-11-23 20:34:23 +0700

    Feat(Room type api) change name api and other

---
+++ server/routes/roomTypes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const router = express.Router();
+const { upload } = require('../helpers/FileHelper');
+
+import {
+    getAllRoomType,
+    getRoomTypeById,
+    createRoomType,
+    updateRoomType,
+    deleteRoomType,
+} from '../controllers/RoomTypesController.js';
+
+router.get('/all', getAllRoomType);
+router.get('/:id', getRoomTypeById);
+router.post('/create', upload.array('images'), createRoomType);
+router.put('/update/:id', upload.array('images'), updateRoomType);
+router.delete('/delete/:id', deleteRoomType);
+
+export default router;
