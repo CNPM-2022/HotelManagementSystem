7a01e311de7713a938e17865f2fb6b8f88db330a
20120557 <81631701+orgball2608@users.noreply.github.com>
2022-12-08 20:45:52 +0700

    Feat(Logout everytime :))))

--- client/src/store/authSlice.js
+++ client/src/store/authSlice.js
@@ -1,39 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialAuthState = {
-  isAuthenticated: false,
-  token: null,
-  user: null,
+    isAuthenticated: false,
+    token: null,
+    user: null,
 };
 
-const localStorageUser = JSON.parse(localStorage.getItem("user"));
+const localStorageUser = JSON.parse(localStorage.getItem('user'));
 
 if (localStorageUser !== null) {
-  initialAuthState.isAuthenticated = true;
-  initialAuthState.user = localStorageUser.user;
-  initialAuthState.token = localStorageUser.accessToken;
+    initialAuthState.isAuthenticated = true;
+    initialAuthState.user = localStorageUser.user;
+    initialAuthState.token = localStorageUser.accessToken;
 } else {
-  initialAuthState.isAuthenticated = false;
-  initialAuthState.user = null;
-  initialAuthState.token = null;
+    initialAuthState.isAuthenticated = false;
+    initialAuthState.user = null;
+    initialAuthState.token = null;
 }
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState: initialAuthState,
-  reducers: {
-    login(state, actions) {
-      state.isAuthenticated = true;
-      state.token = actions.payload.token;
-      state.user = actions.payload.user;
+    name: 'auth',
+    initialState: initialAuthState,
+    reducers: {
+        login(state, actions) {
+            state.isAuthenticated = true;
+            state.token = actions.payload.token;
+            state.user = actions.payload.user;
+        },
+        register(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+        },
     },
-    register(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
 });
 
 export const authActions = authSlice.actions;
