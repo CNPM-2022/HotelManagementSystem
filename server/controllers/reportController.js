4707d74cbc3b1c5686695a5ea56ccdc5d434607f
20120557 <81631701+orgball2608@users.noreply.github.com>
2022-12-11 11:34:33 +0700

    Fix (Create Report) Async and await

--- server/controllers/reportController.js
+++ server/controllers/reportController.js
@@ -2,53 +2,65 @@ import Booking from '../models/Booking';
 import Room from '../models/Room';
 import RoomType from '../models/RoomType';
 import ReportByType from '../models/ReportByRoomType';
-import Report from '../models/Report';
 
 const RestartReport = async () => {
     RoomType.find({}).then(async (roomTypes) => {
-        roomTypes.map(async (roomType) => {
+        for(let i = 0; i < roomTypes.length; i++) {
             await ReportByType.findOne({
-                roomType: roomType._id,
+                roomType: roomTypes[i]._id,
             }).then(async (report) => {
                 if (report) {
                     report.totalRevenue = 0;
-                    report.save();
+                    await report.save();
                 } else {
                     const newReport = new ReportByType({
-                        roomType: roomType._id,
+                        roomType: roomTypes[i]._id,
                         totalRevenue: 0,
                     });
-                    newReport.save();
+                    await newReport.save();
                 }
             });
-        });
+        }
     });
 };
 
 const WriteReport = async () => {
     await RoomType.find({}).then(async (roomTypes) => {
-        roomTypes.map(async (roomType) => {
-            await roomType.listRoom.map(async (room) => {
-                await Room.find({ roomNumber: room }).then(async (Room) => {
+       for(let i = 0; i < roomTypes.length; i++) {
+            const rooms = roomTypes[i].listRoom;
+            for(let i = 0; i < rooms.length; i++) {
+                await Room.find({ roomNumber: rooms[i] }).then(async (Room) => {
                     if (Room[0].currentBookings) {
-                        const total = await Room[0].currentBookings.reduce((total, booking) => {
-                            Booking.find({
-                                _id: booking,
-                            }).then((Book) => {
-                                return total + Book[0].totalAmount;
+                        let totalAmount = 0;
+                        for(let i = 0; i < Room[0].currentBookings.length; i++) {
+                            await Booking.find({ _id: Room[0].currentBookings[i] }).then(async (booking) => {
+                                totalAmount += parseInt(booking[0].totalAmount);
                             });
-                        }, 0);
-                        console.log(total);
+                        }
+                        await ReportByType.findOne({
+                            roomType: roomTypes[i]._id,
+                        }).then(async (report) => {
+                            if (report) {
+                                report.totalRevenue += totalAmount;
+                                await report.save();
+                            } else {
+                                const newReport = new ReportByType({
+                                    roomType: roomTypes[i]._id,
+                                    totalRevenue: totalAmount,
+                                });
+                                await newReport.save();
+                            }
+                        });
                     }
                 });
-            });
-        });
+            }
+        }
     });
 };
 
 const ReportByRoomType = async (req, res) => {
     try {
-        // await RestartReport();
+        await RestartReport();
         await WriteReport();
         ReportByType.find({})
             .populate('roomType', 'typeOfRooms')
