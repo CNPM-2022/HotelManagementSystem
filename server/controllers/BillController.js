b0f5d3b141ac411880f09042806dce6e05491dc5
20120557 <81631701+orgball2608@users.noreply.github.com>
2022-12-12 21:12:14 +0700

    Feat(Bills) fix populate customer

--- server/controllers/BillController.js
+++ server/controllers/BillController.js
@@ -3,8 +3,10 @@ import Bill from '../models/Bill.js';
 const getBills = async (req, res) => {
     try {
         const bills = await Bill.find()
-            .populate({ path: 'booking', populate: { path: 'customerList' }, populate: { path: 'room' } })
+            .populate({ path: 'booking', populate: { path: 'customerList' } })
+            .populate({ path: 'booking', populate: { path: 'room' } })
             .populate('user');
+
         res.status(200).json({
             success: true,
             message: 'Get all bills successfully',
@@ -23,7 +25,8 @@ const getBillById = async (req, res) => {
     try {
         const { id } = req.params;
         const bill = await Bill.findById(id)
-            .populate({ path: 'booking', populate: { path: 'customerList' }, populate: { path: 'room' } })
+            .populate({ path: 'booking', populate: { path: 'customerList' } })
+            .populate({ path: 'booking', populate: { path: 'room' } })
             .populate('user');
         if (!bill) {
             return res.status(404).json({
@@ -50,7 +53,8 @@ const getBillsByUserId = async (req, res) => {
 
     try {
         const bills = await Bill.find({ user: id })
-            .populate({ path: 'booking', populate: { path: 'customerList' }, populate: { path: 'room' } })
+            .populate({ path: 'booking', populate: { path: 'customerList' } })
+            .populate({ path: 'booking', populate: { path: 'room' } })
             .populate('user');
 
         if (bills.length === 0) {
